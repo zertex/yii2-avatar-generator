4bc326df135fdb5ae04f102979c6042d9dba9796
Egorka <error-202@mail.ru>
2018-07-05 18:37:28 +0300

    Update method

--- src/AvatarGenerator.php
+++ src/AvatarGenerator.php
@@ -48,4 +48,23 @@ class AvatarGenerator
 		             ->{($this->text_over_image || $file == null) ? 'text' : '_blank'}()
 		             ->get_file_name();
 	}
+
+	public function update(string $username, int $width = null, string $file = null, string $result_name = null): string
+	{
+		$options = AvatarOptions::create()
+		                        ->setFont(Yii::getAlias($this->font))
+		                        ->setWidth($width ?: $this->size_width)
+		                        ->setFontSize($this->font_size)
+		                        ->setTexturesFolder(Yii::getAlias($this->texture_folder))
+		                        ->setImagesFolder(Yii::getAlias($this->images_folder))
+		                        ->setImagesUrl(Yii::getAlias($this->images_url))
+		                        ->setSalt($this->salt)
+								->setUpdate($this->update());
+
+		return Avatar::init($username, $options, $result_name)
+		             ->{($file==null) ? 'username' : 'file'}($file)
+		             ->{($this->texture_over_image || $file == null) ? 'texture' : '_blank'}($this->texture)
+		             ->{($this->text_over_image || $file == null) ? 'text' : '_blank'}()
+		             ->get_file_name();
+	}
 }
